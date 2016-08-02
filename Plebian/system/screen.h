7048bc780085d6dd3c8c7fc069a2606fa9bf7b05
doc97 <daniel.riissanen@gmail.com>
2016-08-02 19:23:27 +0300

    Refactor scene into screen

---
+++ Plebian/system/screen.h
@@ -0,0 +1,13 @@
+#ifndef ISCREEN_H_
+#define ISCREEN_H_
+
+class IScreen {
+    virtual ~IScreen() = 0;
+    virtual void Enter() = 0;
+    virtual void Exit() = 0;
+    virtual void Show() = 0;
+    virtual void Hide() = 0;
+    virtual void Update(double delta) = 0;
+};
+
+#endif // ISCREEN_H_
