12cffa00a78f8e344c86c1d2b35bc3e26b0917f7
Onni Kosomaa <onni.kosomaa@hotmail.com>
2016-08-10 17:44:52 +0300

    Add missing files to Visual Studio project

--- Plebian/components.h
+++ Plebian/components.h
@@ -5,13 +5,16 @@
 #include <entityx/entityx.h>
 
 #include "renderer/mesh.h"
+#include "renderer/shader.h"
 
 struct Transform : entityx::Component<Transform> {
 	glm::mat4 transform;
 };
 
 struct MeshComponent : entityx::Component<MeshComponent> {
+	MeshComponent(Mesh* mesh, Shader* shader) : mesh(mesh), shader(shader) {}
 	Mesh* mesh = nullptr;
+	Shader* shader = nullptr;
 };
 
 #endif // COMPONENTS_H_
