afb6d7baf936bea62e0d8da45a09c62f5fa353f4
Hugo Holmqvist <hugo.holm@live.com>
2016-11-02 15:50:00 +0200

    Add cubemap texture loading

--- Plebian/io/textureloader.cpp
+++ Plebian/io/textureloader.cpp
@@ -6,11 +6,32 @@
 
 #define TEXTURE_PATH "assets/textures/"
 
-Texture* TextureLoader::GetTexture(std::string fn) {
+Texture* TextureLoader::GetTexture2d(std::string fn, int w, int h, unsigned char** imageData = nullptr) {
 	if (m_textures.count(fn))
 		return m_textures[fn];
 
 	Log(Info, "Loading texture: %s", fn.c_str());
+
+	if (fn.empty()) {
+		Texture* p_tex = new Texture;
+		glEnable(GL_TEXTURE_2D);
+		glGenTextures(1, &p_tex->texture_id);
+		glBindTexture(GL_TEXTURE_2D, p_tex->texture_id);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_SRGB_ALPHA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, &imageData);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+		std::string key = std::to_string(rand() * 99999);
+		while (m_textures[key]) {
+			key = std::to_string(rand() * 99999);
+		}
+
+		m_textures[key] = p_tex;
+		return p_tex;
+	}
 
 	unsigned int width, height;
 	std::vector<unsigned char> data;
@@ -36,4 +57,31 @@ Texture* TextureLoader::GetTexture(std::string fn) {
 
 	m_textures[fn] = p_tex;
 	return p_tex;
+}
+
+Texture* TextureLoader::GetTexture3d(std::string** name, int width, int height, int mips, CubemapData data = nullptr) {
+	Texture* p_tex = new Texture;
+	glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
+	glGenTextures(1, &p_tex->texture_id);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, p_tex->texture_id);
+
+	if (name[0][0].empty()) {
+		for (int i = 0; i < mips; i++) {
+			for (int j = 0; j < 6; j++) {
+				glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + j, i, GL_SRGB_ALPHA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, &data[i][j]);
+
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			}
+		}
+
+		std::string key = std::to_string(rand() * 99999);
+		while (m_textures[key]) {
+			key = std::to_string(rand() * 99999);
+		}
+		m_textures[key] = p_tex;
+		return p_tex;
+	}
 }
--- Plebian/io/textureloader.h
+++ Plebian/io/textureloader.h
@@ -8,9 +8,12 @@
 
 #include "renderer/texture.h"
 
+typedef unsigned char**** CubemapData;
+
 class TextureLoader {
 public:
-	Texture* GetTexture(std::string name);
+	Texture* GetTexture2d(std::string name, int width, int height, unsigned char** data = nullptr);
+	Texture* GetTexture3d(std::string** name, int width, int height, int mips, CubemapData data = nullptr);
 private:
 	std::unordered_map<std::string, Texture*> m_textures;
 };
